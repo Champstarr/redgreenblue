5c6dd03e992a6f850c04f99ddedb53b02abca4fb
Andrew Taylor <champstarr@gmail.com>
2014-11-26 21:49:23 -1000

    changes to redgreenblue

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function () {
+  return gulp.src('./scss/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./css'));
+
+});
+
+gulp.task('watch_styles', function() {
+  gulp.watch('./sass/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles', 'watch_styles']);
